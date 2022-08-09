f1faba47ac86038baf132ae6852e1df637e58d73
Mari Stakhovskaya <maryia.jegorova@gmail.com>
2022-08-09 12:31:33 +0300

    fix comments

--- src/components/comment/Comment.tsx
+++ src/components/comment/Comment.tsx
@@ -1,10 +1,41 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import style from "./Comment.module.css"
+import {json} from "stream/consumers";
+
 
 
 
 
 const Comment = () => {
+
+    type CommentType = {
+        id: number
+        comment: string}
+type CommentsType = Array<CommentType>
+
+const [value, setValue] = useState('')
+    const [comments, setComments] = useState([{}])
+
+
+
+     /* let commentArray =  localStorage.getItem('valueComment')
+        if(commentArray)
+       return  commentArray = JSON.parse(commentArray)
+console.log(commentArray)
+
+
+
+
+
+    const onclickHandler = () => {
+        let newComment = {id:1 , comment:value}
+     setComments([...comments, newComment])
+
+    localStorage.setItem('valueComment',JSON.stringify(comments))
+setValue('')
+
+
+    }*/
 
 
 
@@ -12,7 +43,12 @@ const Comment = () => {
         <div className={style.commentContainer}>
             <h4>Комментарии:</h4>
 
-          <textarea/>
+          <textarea value={value} rows={4} cols={50} onChange={(e)=>{setValue(e.currentTarget.value)}}/>
+            <div>
+            <button >Отправить</button>
+            <button onClick={()=>setValue('')}>Очистить</button>
+            </div>
+{/*<div>{commentArray}</div>*/}
         </div>
     )
 }
